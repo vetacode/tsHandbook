7a15087195658033b9dea28f0ae11a412ce5ec73
vetacode <vetacode@gmail.com>
2025-12-23 14:29:58 +0700

    adds ts compile time vs js runtime error/undefined

--- 1. The Basics/5. tscTypeScriptCompiler.js
+++ 1. The Basics/5. tscTypeScriptCompiler.js
@@ -1,3 +1,7 @@
 //install npm install -g typescript
 // Greets the world.
 console.log('Hello world!');
+function greet(person, date) {
+  console.log('Hello '.concat(person, ', today is ').concat(date, '!'));
+}
+console.log(greet('Brendan')); //its not shows error, rather resulting undefined when code runs
--- 1. The Basics/5. tscTypeScriptCompiler.ts
+++ 1. The Basics/5. tscTypeScriptCompiler.ts
@@ -1,3 +1,9 @@
 //install npm install -g typescript
 // Greets the world.
 console.log('Hello world!');
+
+function greet(person, date) {
+  console.log(`Hello ${person}, today is ${date}!`);
+}
+
+greet('Brendan'); // In TypeScript, errors are detected at compile time, so we can avoid runtime errors before running the code.
