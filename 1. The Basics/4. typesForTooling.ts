68851bcd6f2a7381633aafe065216729ec71dd96
vetacode <vetacode@gmail.com>
2025-12-23 12:29:46 +0700

    adds remarks on type tooling

--- 1. The Basics/4. typesForTooling.ts
+++ 1. The Basics/4. typesForTooling.ts
@@ -4,7 +4,7 @@ import express, { Request, Response } from 'express';
 const app = express();
 
 app.get('/', function (req: Request, res: Response) {
-  res.sen; //Property 'sen' does not exist on type 'Response<any, Record<string, any>>'. Did you mean 'send'?
+  res.sen; //Property 'sen' does not exist on type 'Response<any, Record<string, any>>'. Did you mean 'send'? and it gives type checker code completion/suggestions at runtime
   //
 });
 
