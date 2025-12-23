acae03cd2f0157a79d45658762f339b73bc11374
vetacode <vetacode@gmail.com>
2025-12-23 20:45:58 +0700

    adds strictness for tsconfig.json and cli

---
+++ 1. The Basics/10. strictness.ts
@@ -0,0 +1,4 @@
+// TypeScript has several type-checking strictness flags that can be turned on or off, and all of our examples will be written with all of them enabled unless otherwise stated.
+// The strict flag in the CLI, or "strict": true in a tsconfig.json toggles them all on simultaneously
+// We can opt out of them individually.
+// The two biggest ones you should know about are noImplicitAny and strictNullChecks.
