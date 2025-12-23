35f57fcdadc6d504123e560af7867b99ec926fd6
vetacode <vetacode@gmail.com>
2025-12-23 14:37:32 +0700

    adds emitting with errors

---
+++ 1. The Basics/6. emittingWithErrors.js
@@ -0,0 +1,3 @@
+// Greets the world.
+console.log('Hello world!');
+//use: tsc --noEmitOnError hello.ts to stop compile to JS files
---
+++ 1. The Basics/6. emittingWithErrors.ts
@@ -0,0 +1,11 @@
+// Greets the world.
+console.log('Hello world!');
+
+//use: tsc --noEmitOnError hello.ts to stop compile to JS files
+function greet(person, date) {
+  console.log(`Hello ${person}, today is ${date}!`);
+}
+
+greet('Brendan');
+
+//tsc --noEmitOnError hello.ts will not produce updated JS files
