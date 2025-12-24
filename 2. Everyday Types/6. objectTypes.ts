8ef44e7a69b1f11150704d449cb55731ade93273
vetacode <vetacode@gmail.com>
2025-12-24 21:50:37 +0700

    adds optional properties

--- 2. Everyday Types/6. objectTypes.ts
+++ 2. Everyday Types/6. objectTypes.ts
@@ -1,6 +1,14 @@
-// The parameter's type annotation is an object type
+//1. The parameter's type annotation is an object type
 function printCoord(pt: { x: number; y: number }) {
   console.log("The coordinate's x value is " + pt.x);
   console.log("The coordinate's y value is " + pt.y);
 }
 printCoord({ x: 3, y: 7 });
+
+//2. Optional Properties
+function printName(obj: { first: string; last?: string }) {
+  // ...
+}
+// Both OK
+printName({ first: 'Bob' });
+printName({ first: 'Alice', last: 'Alisson' });
