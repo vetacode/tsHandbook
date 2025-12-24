8534ebb9004b0876e3da6d102418fd7fa0031deb
vetacode <vetacode@gmail.com>
2025-12-24 20:45:40 +0700

    adds primitives

---
+++ 2. Everyday Types/1. primitives.ts
@@ -0,0 +1,5 @@
+// string represents string values like "Hello, world"
+// number is for numbers like 42. JavaScript does not have a special runtime value for integers, so there’s no equivalent to int or float - everything is simply number
+// boolean is for the two values true and false
+
+// The type names String, Number, and Boolean (starting with capital letters) are legal, but refer to some special built-in types that will very rarely appear in your code. Always use string, number, or boolean for types.
