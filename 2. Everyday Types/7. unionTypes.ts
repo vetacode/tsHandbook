32740033c58433c9a6b349ce7caa6cff6b104285
vetacode <vetacode@gmail.com>
2025-12-25 13:00:12 +0700

    adds working wiht union type

--- 2. Everyday Types/7. unionTypes.ts
+++ 2. Everyday Types/7. unionTypes.ts
@@ -8,3 +8,16 @@ printId('202');
 // Error
 printId({ myID: 22342 });
 // Argument of type '{ myID: number; }' is not assignable to parameter of type 'string | number'.
+
+function printTextOrNumberOrBool(
+  textOrNumberOrBool: string | number | boolean
+) {
+  console.log(textOrNumberOrBool);
+}
+
+//has to use the method that available for both union type
+function printId(id: number | string) {
+  console.log(id.toUpperCase());
+  // Property 'toUpperCase' does not exist on type 'string | number'.
+  //   Property 'toUpperCase' does not exist on type 'number'.
+}
