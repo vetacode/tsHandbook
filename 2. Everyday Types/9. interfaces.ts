fe83108b116ee37fe9a63e58c250de52b8443436
vetacode <vetacode@gmail.com>
2025-12-25 14:06:54 +0700

    adds extending via interfaces vs types

--- 2. Everyday Types/9. interfaces.ts
+++ 2. Everyday Types/9. interfaces.ts
@@ -12,3 +12,45 @@ printCoord({ x: 100, y: 100 });
 
 //Differences Between Type Aliases and Interfaces
 // Almost all features of an interface are available in type, the key distinction is that a type cannot be re-opened to add new properties vs an interface which is always extendable.
+
+//1. Extending interface
+interface Animal {
+  name: string;
+}
+
+interface Bear extends Animal {
+  honey: boolean;
+}
+
+function getBear(): Bear {
+  return {
+    name: 'Winny',
+    honey: true,
+  };
+}
+
+const bear = getBear();
+bear.name;
+bear.honey;
+
+//2. Extending type
+{
+  type Animal = {
+    name: string;
+  };
+
+  type Bear = Animal & {
+    honey: boolean;
+  };
+
+  function getBear(): Bear {
+    return {
+      name: 'Winny',
+      honey: true,
+    };
+  }
+
+  const bear = getBear();
+  bear.name;
+  bear.honey;
+}
