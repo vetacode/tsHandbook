ad3167b622832ed3729dba82e420ca13f8579906
vetacode <vetacode@gmail.com>
2025-12-25 12:55:25 +0700

    adds union types

---
+++ 2. Everyday Types/7. unionTypes.ts
@@ -0,0 +1,10 @@
+function printId(id: number | string) {
+  console.log('Your ID is: ' + id);
+}
+// OK
+printId(101);
+// OK
+printId('202');
+// Error
+printId({ myID: 22342 });
+// Argument of type '{ myID: number; }' is not assignable to parameter of type 'string | number'.
