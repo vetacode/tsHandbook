b44ea5c7062897943b4375c0898146f753a2c0f3
vetacode <vetacode@gmail.com>
2025-12-26 22:26:13 +0700

    adds enums

---
+++ 2. Everyday Types/13. enums.ts
@@ -0,0 +1,16 @@
+//Enums are a feature added to JavaScript by TypeScript which allows for describing a value which could be one of a set of possible named constants.
+enum Direction {
+  Up = 1,
+  Down,
+  Left,
+  Right,
+}
+
+enum Direction2 {
+  Up,
+  Down,
+  Left,
+  Right,
+}
+
+console.log(typeof Direction);
