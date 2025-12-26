a9909c91ff9ff54a6e2a8dea6a1e07027500fa3c
vetacode <vetacode@gmail.com>
2025-12-26 22:34:54 +0700

    adds bigint

---
+++ 2. Everyday Types/14. lessCommonPrimitives.ts
@@ -0,0 +1,7 @@
+//BigInt: is a primitive in JavaScript used for very large integers
+
+// Creating a bigint via the BigInt function
+const oneHundred: bigint = BigInt(100);
+
+// Creating a BigInt via the literal syntax
+const anotherHundred: bigint = 100n;
