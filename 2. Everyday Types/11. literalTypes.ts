d2c7f4a602be37b876f7abe5baa60974c8508b8e
vetacode <vetacode@gmail.com>
2025-12-26 20:42:22 +0700

    adds combining literal union

--- 2. Everyday Types/11. literalTypes.ts
+++ 2. Everyday Types/11. literalTypes.ts
@@ -16,11 +16,9 @@ let x: 'hello' = 'hello'; // OK
 x = 'hello'; // ...
 x = 'howdy'; // Type '"howdy"' is not assignable to type '"hello"'.
 
-
-//Union
-function printText(s: string, alignment: "left" | "right" | "center") {
+//Combining literal Union
+function printText(s: string, alignment: 'left' | 'right' | 'center') {
   // ...
 }
-printText("Hello, world", "left");
-printText("G'day, mate", "centre");
-Argument of type '"centre"' is not assignable to parameter of type '"left" | "right" | "center"'.
+printText('Hello, world', 'left');
+printText("G'day, mate", 'centre'); // Argument of type '"centre"' is not assignable to parameter of type '"left" | "right" | "center"'.
