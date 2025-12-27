284e2228f82962502e09cbd699506286886e1117
vetacode <vetacode@gmail.com>
2025-12-27 23:27:37 +0700

    adds using type predicates

--- 3. Narrowing/9. usingTypePredicates.ts
+++ 3. Narrowing/9. usingTypePredicates.ts
@@ -1,5 +1,5 @@
-type Fish = { swim: () => void };
-type Bird = { fly: () => void };
+// type Fish = { swim: () => void };
+// type Bird = { fly: () => void };
 declare function getSmallPet(): Fish | Bird;
 // ---cut---
 function isFish(pet: Fish | Bird): pet is Fish {
@@ -14,3 +14,18 @@ if (isFish(pet)) {
 } else {
   pet.fly();
 }
+
+type Fish = { swim: () => void; name: string };
+type Bird = { fly: () => void; name: string };
+
+// ---cut---
+const zoo: (Fish | Bird)[] = [getSmallPet(), getSmallPet(), getSmallPet()];
+const underWater1: Fish[] = zoo.filter(isFish);
+// or, equivalently
+const underWater2: Fish[] = zoo.filter(isFish) as Fish[];
+
+// The predicate may need repeating for more complex examples
+const underWater3: Fish[] = zoo.filter((pet): pet is Fish => {
+  if (pet.name === 'sharkey') return false;
+  return isFish(pet);
+});
