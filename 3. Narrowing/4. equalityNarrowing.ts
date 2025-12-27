f6bc8887b8357a3b8c3c7552fadcdc6af38f67b5
vetacode <vetacode@gmail.com>
2025-12-27 12:39:58 +0700

    adds checking undefined value

--- 3. Narrowing/4. equalityNarrowing.ts
+++ 3. Narrowing/4. equalityNarrowing.ts
@@ -31,3 +31,20 @@ function printAll(strs: string | string[] | null) {
     }
   }
 }
+
+//checking undefined
+interface Container {
+  value: number | null | undefined;
+}
+
+function multiplyValue(container: Container, factor: number) {
+  // Remove both 'null' and 'undefined' from the type.
+  if (container.value != null) {
+    console.log(container.value);
+
+    // (property) Container.value: number
+
+    // Now we can safely multiply 'container.value'.
+    container.value *= factor;
+  }
+}
