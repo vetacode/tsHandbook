9e3a8187445385d296a0b288afb9b11565ceaa44
vetacode <vetacode@gmail.com>
2025-12-27 10:45:19 +0700

    adds typeof type guards

---
+++ 3. Narrowing/2. typeofTypeguards.ts
@@ -0,0 +1,23 @@
+// TypeScript expects this return (a certain set of strings) from typeof:
+'string';
+'number';
+'bigint';
+'boolean';
+'symbol';
+'undefined';
+'object';
+'function';
+
+//Type Guard is checking against the value returned by typeof
+function printAll(strs: string | string[] | null) {
+  if (typeof strs === 'object') {
+    for (const s of strs) {
+      // 'strs' is possibly 'null'.
+      console.log(s);
+    }
+  } else if (typeof strs === 'string') {
+    console.log(strs);
+  } else {
+    // do nothing
+  }
+}
