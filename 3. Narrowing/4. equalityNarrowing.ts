f486bd31d57e4c779f0c45454701f3889fbbc485
vetacode <vetacode@gmail.com>
2025-12-27 12:32:18 +0700

    adds not equal to !== narrowing

--- 3. Narrowing/4. equalityNarrowing.ts
+++ 3. Narrowing/4. equalityNarrowing.ts
@@ -16,3 +16,18 @@ function example(x: string | number, y: string | boolean) {
     // (parameter) y: string | boolean
   }
 }
+
+function printAll(strs: string | string[] | null) {
+  if (strs !== null) {
+    if (typeof strs === 'object') {
+      for (const s of strs) {
+        // (parameter) strs: string[]
+        console.log(s);
+      }
+    } else if (typeof strs === 'string') {
+      console.log(strs);
+
+      // (parameter) strs: string
+    }
+  }
+}
