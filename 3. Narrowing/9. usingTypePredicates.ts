6178fbdeeffa1a09e6ac1e07a4beef1f41df179e
vetacode <vetacode@gmail.com>
2025-12-27 22:48:03 +0700

    adds pet getSmallPer()

--- 3. Narrowing/9. usingTypePredicates.ts
+++ 3. Narrowing/9. usingTypePredicates.ts
@@ -5,3 +5,12 @@ declare function getSmallPet(): Fish | Bird;
 function isFish(pet: Fish | Bird): pet is Fish {
   return (pet as Fish).swim !== undefined;
 }
+
+// Both calls to 'swim' and 'fly' are now okay.
+let pet = getSmallPet();
+
+if (isFish(pet)) {
+  pet.swim();
+} else {
+  pet.fly();
+}
