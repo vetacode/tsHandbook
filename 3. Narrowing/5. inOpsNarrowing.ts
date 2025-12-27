58994e4b742ec675d8ccc0916de08ac512bcf243
vetacode <vetacode@gmail.com>
2025-12-27 21:59:37 +0700

    adds type that has both props

--- 3. Narrowing/5. inOpsNarrowing.ts
+++ 3. Narrowing/5. inOpsNarrowing.ts
@@ -14,3 +14,22 @@ move({
     console.log('Fish is swimming');
   },
 });
+
+{
+  //add one more type
+  type Fish = { swim: () => void };
+  type Bird = { fly: () => void };
+  type Human = { swim?: () => void; fly?: () => void };
+
+  function move(animal: Fish | Bird | Human) {
+    if ('swim' in animal) {
+      animal;
+
+      // (parameter) animal: Fish | Human
+    } else {
+      animal;
+
+      // (parameter) animal: Bird | Human
+    }
+  }
+}
