96092b178f50112e77493653dbab863d7fa0d448
vetacode <vetacode@gmail.com>
2025-12-27 22:44:15 +0700

    adds using type predicates

---
+++ 3. Narrowing/9. usingTypePredicates.ts
@@ -0,0 +1,7 @@
+type Fish = { swim: () => void };
+type Bird = { fly: () => void };
+declare function getSmallPet(): Fish | Bird;
+// ---cut---
+function isFish(pet: Fish | Bird): pet is Fish {
+  return (pet as Fish).swim !== undefined;
+}
