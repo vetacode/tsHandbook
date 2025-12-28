0977b3ba82a30314178449e116ba9d9e98e52450
vetacode <vetacode@gmail.com>
2025-12-28 19:24:14 +0700

    adds combine call construct signatures

--- 4. More on Functions/3. constructSignatures.ts
+++ 4. More on Functions/3. constructSignatures.ts
@@ -6,3 +6,23 @@ type SomeConstructor = {
 function fn(ctor: SomeConstructor) {
   return new ctor('hello');
 }
+
+//combine call and construct signatures in the same type
+interface CallOrConstruct {
+  (n?: number): string;
+  new (s: string): Date;
+}
+
+function func(ctor: CallOrConstruct) {
+  // Passing an argument of type `number` to `ctor` matches it against
+  // the first definition in the `CallOrConstruct` interface.
+  console.log(ctor(10));
+  // ^?
+
+  // Similarly, passing an argument of type `string` to `ctor` matches it
+  // against the second definition in the `CallOrConstruct` interface.
+  console.log(new ctor('10'));
+  // ^?
+}
+
+func(Date);
