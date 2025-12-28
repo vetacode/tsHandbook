5d12e489c6bb736e91d4ec9b59818350e31a4733
vetacode <vetacode@gmail.com>
2025-12-28 10:11:10 +0700

    adds never type

---
+++ 3. Narrowing/5. neverType.ts
@@ -0,0 +1,1 @@
+// When narrowing, you can reduce the options of a union to a point where you have removed all possibilities and have nothing left. In those cases, TypeScript will use a never type to represent a state which shouldn’t exist.
