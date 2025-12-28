59d032d89cd4ee8d2d0f41180b84fd488972957c
vetacode <vetacode@gmail.com>
2025-12-28 20:33:14 +0700

    adds remarks on generics applied

--- 4. More on Functions/4. genericFunctions.ts
+++ 4. More on Functions/4. genericFunctions.ts
@@ -2,10 +2,11 @@ function firstElement(arr: any[]) {
   return arr[0];
 }
 console.log(firstElement([1, 3, 5, 7, 9])); //1
+// tapi: function firstElement(arr: any[]): any
 
 // generics are used when we want to describe a correspondence between two values. We do this by declaring a type parameter in the function signature:
 function firstElement2<T>(arr: T[]): T | undefined {
   return arr[0];
 }
-
-console.log(firstElement2(['coba', 'array', 'of', 'strings'])); //coba
+console.log(firstElement2(['coba', 'array', 'of', 'strings'])); //coba.
+// type: function firstElement2<string>(arr: string[]): string | undefined
