b6d6fd59a8b7e21ed31c7b90b65ee192bd3cef78
vetacode <vetacode@gmail.com>
2025-12-28 19:39:50 +0700

    adds remark on construct signatures

--- 4. More on Functions/3. constructSignatures.ts
+++ 4. More on Functions/3. constructSignatures.ts
@@ -17,12 +17,14 @@ function func(ctor: CallOrConstruct) {
   // Passing an argument of type `number` to `ctor` matches it against
   // the first definition in the `CallOrConstruct` interface.
   console.log(ctor(10));
-  // ^?
+  // ^(parameter) ctor: CallOrConstruct
+  // (n?: number | undefined) => string
 
   // Similarly, passing an argument of type `string` to `ctor` matches it
   // against the second definition in the `CallOrConstruct` interface.
   console.log(new ctor('10'));
-  // ^?
+  // ^(parameter) ctor: CallOrConstruct
+  // new (s: string) => Date
 }
 
 func(Date);
