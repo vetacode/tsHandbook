9ed48bec4f6d4537591561cc1f0a9b5bf91ba4a2
vetacode <vetacode@gmail.com>
2025-12-28 10:14:46 +0700

    adds new interfaces will shows error

--- 3. Narrowing/6. exhaustivenessChecking.ts
+++ 3. Narrowing/6. exhaustivenessChecking.ts
@@ -19,3 +19,25 @@ function getArea(shape: Shape) {
       return _exhaustiveCheck;
   }
 }
+
+{
+  //adding new member will shows error:
+  interface Triangle {
+    kind: 'triangle';
+    sideLength: number;
+  }
+
+  type Shape = Circle | Square | Triangle; //adds new interfaces 'Triangle' will results error
+
+  function getArea(shape: Shape) {
+    switch (shape.kind) {
+      case 'circle':
+        return Math.PI * shape.radius ** 2;
+      case 'square':
+        return shape.sideLength ** 2;
+      default:
+        const _exhaustiveCheck: never = shape; // Type 'Triangle' is not assignable to type 'never'.
+        return _exhaustiveCheck;
+    }
+  }
+}
