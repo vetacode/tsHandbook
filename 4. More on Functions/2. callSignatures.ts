2516ff58ad6dd6e2fa8f53405a200e035b1be439
vetacode <vetacode@gmail.com>
2025-12-28 19:15:33 +0700

    adds remarks of call signatures

--- 4. More on Functions/2. callSignatures.ts
+++ 4. More on Functions/2. callSignatures.ts
@@ -1,3 +1,8 @@
+// Call signature = cara TypeScript mendeskripsikan object yang bisa dipanggil
+// Digunakan saat function punya properti
+// Ditulis di dalam object type
+// Sangat berguna untuk API design yang idiomatik JavaScript
+
 type DescribableFunction = {
   description: string;
   (someArg: number): boolean; //call signature in an object type
@@ -14,3 +19,12 @@ myFunc.description = 'default description';
 console.log(myFunc); //[Function: myFunc] { description: 'default description' }
 
 doSomething(myFunc);
+
+// Gunakan call signatures jika:
+//    fungsi punya property tambahan
+//    mengikuti pola JavaScript “function as object”
+//    membuat API fleksibel (plugin, hook, middleware, validator)
+
+// Ga perlu call signature jika:
+//    fungsi hanya dipanggil tanpa property
+//    cukup dengan (args) => returnType
