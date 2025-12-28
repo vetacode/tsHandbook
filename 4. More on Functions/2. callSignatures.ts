6fcc3eea07f6249946fa6349d270324dc944cfa4
vetacode <vetacode@gmail.com>
2025-12-28 19:10:04 +0700

    adds logs on myFunc

--- 4. More on Functions/2. callSignatures.ts
+++ 4. More on Functions/2. callSignatures.ts
@@ -1,14 +1,16 @@
 type DescribableFunction = {
   description: string;
-  (someArg: number): boolean;
+  (someArg: number): boolean; //call signature in an object type
 };
 function doSomething(fn: DescribableFunction) {
-  console.log(fn.description + ' returned ' + fn(6));
+  console.log(fn.description + ' returned ' + fn(6)); //default description returned true
 }
 
 function myFunc(someArg: number) {
   return someArg > 3;
 }
 myFunc.description = 'default description';
+
+console.log(myFunc); //[Function: myFunc] { description: 'default description' }
 
 doSomething(myFunc);
