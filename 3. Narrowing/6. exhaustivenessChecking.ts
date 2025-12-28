e78c4289c2bcea75d386bc33142a312414bbcdef
vetacode <vetacode@gmail.com>
2025-12-28 10:12:42 +0700

    adds exhaustiveness checking

---
+++ 3. Narrowing/6. exhaustivenessChecking.ts
@@ -0,0 +1,21 @@
+interface Circle {
+  kind: 'circle';
+  radius: number;
+}
+interface Square {
+  kind: 'square';
+  sideLength: number;
+}
+type Shape = Circle | Square;
+
+function getArea(shape: Shape) {
+  switch (shape.kind) {
+    case 'circle':
+      return Math.PI * shape.radius ** 2;
+    case 'square':
+      return shape.sideLength ** 2;
+    default:
+      const _exhaustiveCheck: never = shape;
+      return _exhaustiveCheck;
+  }
+}
