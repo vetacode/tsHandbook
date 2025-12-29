c1d1cf53e5a00bc858f2a2867b0e969c723ee422
vetacode <vetacode@gmail.com>
2025-12-30 06:42:19 +0700

    adds ? to add undefined type

--- 4. More on Functions/5. optionalParameters.ts
+++ 4. More on Functions/5. optionalParameters.ts
@@ -1,4 +1,11 @@
-function f(n: number) {
+function fn(n: number) {
   console.log(n.toFixed()); // 0 arguments
   console.log(n.toFixed(3)); // 1 argument
 }
+
+function f(x?: number) {
+  // ? marks optional
+  // ...
+}
+f(); // OK
+f(10); // OK
