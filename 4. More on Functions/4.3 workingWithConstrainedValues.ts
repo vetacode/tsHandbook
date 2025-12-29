d1d66a063aa7ad50c1c99edd474c3a49ff52f9ee
vetacode <vetacode@gmail.com>
2025-12-29 12:37:35 +0700

    adds working with constrained val

---
+++ 4. More on Functions/4.3 workingWithConstrainedValues.ts
@@ -0,0 +1,12 @@
+function minimumLength<Type extends { length: number }>(
+  obj: Type,
+  minimum: number
+): Type {
+  if (obj.length >= minimum) {
+    return obj;
+  } else {
+    return { length: minimum };
+    // Type '{ length: number; }' is not assignable to type 'Type'.
+    //   '{ length: number; }' is assignable to the constraint of type 'Type', but 'Type' could be instantiated with a different subtype of constraint '{ length: number; }'.
+  }
+}
