fe2ff22e70cba6b47c07052758c7172e4ae35857
vetacode <vetacode@gmail.com>
2025-12-29 18:14:44 +0700

    adds guideline writing good generic functions

---
+++ 4. More on Functions/4.5 guidelinesGoodGenericFns.ts
@@ -0,0 +1,29 @@
+// 1. Push Type Parameters Down
+//    Here are two ways of writing a function that appear similar:
+//    Rule: When possible, use the type parameter itself rather than constraining it
+function firstElement1<Type>(arr: Type[]) {
+  return arr[0];
+}
+
+function firstElement2<Type extends any[]>(arr: Type) {
+  return arr[0];
+}
+
+// a: number (good)
+const a = firstElement1([1, 2, 3]);
+// b: any (bad)
+const b = firstElement2([1, 2, 3]);
+
+//2. Use Fewer Type Parameters
+//    Here’s another pair of similar functions:
+
+function filter1<Type>(arr: Type[], func: (arg: Type) => boolean): Type[] {
+  return arr.filter(func);
+}
+
+function filter2<Type, Func extends (arg: Type) => boolean>(
+  arr: Type[],
+  func: Func
+): Type[] {
+  return arr.filter(func);
+}
