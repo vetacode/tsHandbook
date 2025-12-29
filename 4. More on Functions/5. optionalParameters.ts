323110e305600777c83f928f42c93675a304adea
vetacode <vetacode@gmail.com>
2025-12-30 06:48:01 +0700

    passing undefined argument on optional params

--- 4. More on Functions/5. optionalParameters.ts
+++ 4. More on Functions/5. optionalParameters.ts
@@ -3,17 +3,23 @@ function fn(n: number) {
   console.log(n.toFixed(3)); // 1 argument
 }
 
+fn(undefined); //Error: Argument of type 'undefined' is not assignable to parameter of type 'number'.
+
+//Solution
+//1. Adds ?
 function f(x?: number) {
   // ? marks optional => adds undefined
   // ...
 }
 f(); // OK
 f(10); // OK
+f(undefined); //OK
 
-//can also provide parameter default
+//2. provide parameter default
 function fx(x = 10) {
   // ...
 }
 
-fx();
-fx(5);
+fx(); // OK
+fx(5); // OK
+fx(undefined); // OK
