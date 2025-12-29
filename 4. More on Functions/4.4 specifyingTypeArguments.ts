b148c4cc107eacbeb374c9f63c475a2017b514b4
vetacode <vetacode@gmail.com>
2025-12-29 17:48:14 +0700

    adds the errors and the solution

--- 4. More on Functions/4.4 specifyingTypeArguments.ts
+++ 4. More on Functions/4.4 specifyingTypeArguments.ts
@@ -2,5 +2,8 @@ function combine<T>(arr1: T[], arr2: T[]): T[] {
   return arr1.concat(arr2);
 }
 
-// const arr = combine([1, 2, 3], ['hello']); //
-const arr = combine(['hello'], [1, 2, 3]); //Type 'number' is not assignable to type 'string'.
+// const arr = combine([1, 2, 3], ['hello']); //Type 'string' is not assignable to type 'number'.
+// const arr = combine(['hello'], [1, 2, 3]); //Type 'number' is not assignable to type 'string'.
+
+const arr = combine([10, 20, 30], [1, 2, 3, 4]);
+console.log(arr); //[10, 20, 30, 1, 2,  3,  4]
