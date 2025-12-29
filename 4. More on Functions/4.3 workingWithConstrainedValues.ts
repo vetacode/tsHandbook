88e03c068351ddbcbb7cae0a90fdf71f5ff0ce9a
vetacode <vetacode@gmail.com>
2025-12-29 13:00:02 +0700

    adds solution to contraints

--- 4. More on Functions/4.3 workingWithConstrainedValues.ts
+++ 4. More on Functions/4.3 workingWithConstrainedValues.ts
@@ -19,4 +19,23 @@ console.log(minimumLength('hello', 10)); //{ length: 10 }
 
 const arr = minimumLength([1, 2, 3], 6);
 console.log(arr); //{ length: 6 }
-console.log(arr.slice(0)); // TypeError: arr.slice is not a function
+// console.log(arr.slice(0)); // TypeError: arr.slice is not a function
+
+//solution
+interface P {
+  length: number;
+}
+
+function minLength<T extends P>(obj: T, minimum: number): T | P {
+  if (obj.length >= minimum) {
+    return obj;
+  } else {
+    return { length: minimum };
+  }
+}
+
+console.log(minLength('hello', 10)); //{ length: 10 }
+
+const arr2 = minLength([1, 2, 3], 6);
+console.log(arr2); //{ length: 6 }
+// console.log(arr2.slice(0));
