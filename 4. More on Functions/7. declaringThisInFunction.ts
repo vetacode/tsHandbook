ee3f76e7f431d9f6e777370e47e642a4fd4c05b9
vetacode <vetacode@gmail.com>
2025-12-30 14:41:51 +0700

    adds declare type for this in the function body

--- 4. More on Functions/7. declaringThisInFunction.ts
+++ 4. More on Functions/7. declaringThisInFunction.ts
@@ -9,5 +9,23 @@ const user = {
 console.log(user.admin); //false
 
 user.becomeAdmin();
-
 console.log(user.admin); //true
+
+//Declare type for 'this' in the function body
+
+interface User {
+  id: number;
+  admin: boolean;
+}
+declare const getDB: () => DB;
+// ---cut---
+interface DB {
+  filterUsers(filter: (this: User) => boolean): User[];
+}
+
+const db = getDB();
+const admins = db.filterUsers(function (this: User) {
+  return this.admin;
+});
+
+console.log(admins);
