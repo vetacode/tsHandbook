12b3d7a156ece96bca145052c1f8a32888d6ef2f
vetacode <vetacode@gmail.com>
2025-12-30 13:16:45 +0700

    adds Overload Signatures and the Implementation Signature

--- 4. More on Functions/6. functionOverloads.ts
+++ 4. More on Functions/6. functionOverloads.ts
@@ -14,3 +14,19 @@ console.log(d2);
 
 const d3 = makeDate(1, 3); // No overload expects 2 arguments, but overloads do exist that expect either 1 or 3 arguments.
 console.log(d3); //1970-01-01T00:00:00.001Z
+
+//Overload Signatures and the Implementation Signature
+
+//NOTES:
+// The signature of the implementation is not visible from the outside. When writing an overloaded function, you should always have two or more signatures above the implementation of the function.
+
+// 1️⃣ overload signatures (public, terlihat dari luar)
+function fn(x: string): string;
+function fn(x: number): number;
+
+// 2️⃣ implementation signature (private, tidak terlihat dari luar)
+function fn(x: string | number) {
+  return typeof x === 'string' ? x.toUpperCase() : x * 2;
+}
+
+console.log(fn('Hello World of Andromeda Galaxy')); //HELLO WORLD OF ANDROMEDA GALAXY
