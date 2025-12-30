6db5478cbfaaf471295a89f34e368ff6381e1127
vetacode <vetacode@gmail.com>
2025-12-30 07:43:01 +0700

    adds logs on myForEach3

--- 4. More on Functions/5.1 opsParamsInCallbacks.ts
+++ 4. More on Functions/5.1 opsParamsInCallbacks.ts
@@ -1,3 +1,5 @@
+//MISTAKES
+//Aman di runtime, tapi TIDAK optimal secara type design.
 function myForEach(arr: any[], callback: (arg: any, index?: number) => void) {
   for (let i = 0; i < arr.length; i++) {
     callback(arr[i], i);
@@ -14,9 +16,6 @@ myForEach([1, 2, 3], (a, i) => console.log(a, i));
 // 2 1
 // 3 2
 
-// Rule: When writing a function type for a callback, never write an optional parameter unless you intend to call the function without passing that argument
-
-//MISTAKES
 function myForEach2(arr: any[], callback: (arg: any, index?: number) => void) {
   for (let i = 0; i < arr.length; i++) {
     // I don't feel like providing the index today
@@ -24,7 +23,22 @@ function myForEach2(arr: any[], callback: (arg: any, index?: number) => void) {
   }
 }
 
-myForEach2([1, 2, 3], (a, i) => {
-  console.log(i.toFixed());
-  // 'i' is possibly 'undefined'.
+// myForEach2([1, 2, 3], (a, i) => {
+//   console.log(i.toFixed());
+//   // 'i' is possibly 'undefined'.
+// });
+
+// Rule: When writing a function type for a callback, never write an optional parameter unless you intend to call the function without passing that argument
+
+//Solution (ideal)
+function myForEach3(arr: any[], callback: (arg: any, index: number) => void) {
+  for (let i = 0; i < arr.length; i++) {
+    callback(arr[i], i);
+  }
+}
+myForEach3([1, 2, 3], (a, i) => {
+  console.log(a, i, i.toFixed()); //aman semua
 });
+// 1 0 0
+// 2 1 1
+// 3 2 2
