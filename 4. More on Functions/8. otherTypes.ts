ad62c51b5903aff8cde15b067ae5a258dd20852e
vetacode <vetacode@gmail.com>
2025-12-30 22:28:35 +0700

    adds unknown

--- 4. More on Functions/8. otherTypes.ts
+++ 4. More on Functions/8. otherTypes.ts
@@ -10,3 +10,14 @@ function noop() {
 //    object is not Object. Always use object!
 //    Object is a global type
 //    function types are considered to be objects in TypeScript.
+
+//3. unknown: represents any value.
+//    similar to the 'any' type, but 'unknown' is safer coz it is illegal to do anything with unknown value (the TS will always scream)
+
+function f1(a: any) {
+  a.b(); // OK
+}
+function f2(a: unknown) {
+  a.b();
+  // 'a' is of type 'unknown'.
+}
