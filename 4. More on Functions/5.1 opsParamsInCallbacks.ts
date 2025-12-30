23725ed63271aa42de7181cb1c7d0fa8700c58b4
vetacode <vetacode@gmail.com>
2025-12-30 07:22:06 +0700

    adds remarks rule on opsParamsInCallbacks

--- 4. More on Functions/5.1 opsParamsInCallbacks.ts
+++ 4. More on Functions/5.1 opsParamsInCallbacks.ts
@@ -4,3 +4,5 @@ function myForEach(arr: any[], callback: (arg: any, index?: number) => void) {
     callback(arr[i], i);
   }
 }
+
+// Rule: When writing a function type for a callback, never write an optional parameter unless you intend to call the function without passing that argument
