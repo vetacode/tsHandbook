55d4381b1387ca9c3cdf50d5979f7f06b059db7a
vetacode <vetacode@gmail.com>
2025-12-30 22:22:28 +0700

    adds void

---
+++ 4. More on Functions/8. otherTypes.ts
@@ -0,0 +1,7 @@
+//1. VOID: represents the return value of functions which don’t return a value.
+//    void is not the same as undefined.
+// The inferred return type is void
+function noop() {
+  //function noop(): void
+  return;
+}
