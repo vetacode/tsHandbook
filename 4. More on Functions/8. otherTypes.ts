0e62fba69ba7bf8ee24bcd9523e6de2fe7ef08c4
vetacode <vetacode@gmail.com>
2025-12-30 22:25:42 +0700

    adds object

--- 4. More on Functions/8. otherTypes.ts
+++ 4. More on Functions/8. otherTypes.ts
@@ -1,7 +1,12 @@
-//1. VOID: represents the return value of functions which don’t return a value.
+//1. void represents the return value of functions which don’t return a value.
 //    void is not the same as undefined.
 // The inferred return type is void
 function noop() {
   //function noop(): void
   return;
 }
+
+//2. object: a special type refers to any value that isn’t a primitive (string, number, bigint, boolean, symbol, null, or undefined)
+//    object is not Object. Always use object!
+//    Object is a global type
+//    function types are considered to be objects in TypeScript.
