240f4cee18e945f5246b6dbd873538793e555174
vetacode <vetacode@gmail.com>
2025-12-31 11:53:45 +0700

    adds edge cases on void

--- 4. More on Functions/8.1 voidUndefinedNeverDiff.ts
+++ 4. More on Functions/8.1 voidUndefinedNeverDiff.ts
@@ -6,3 +6,17 @@ function log(msg: string): void {
 //function boleh return sesuatu secara runtime tapi caller ga boleh pakai hasil return nya:
 const x = log('Hello');
 x.toString(); //Error: Property 'toString' does not exist on type 'void'.
+
+//Edge case: callback void
+const cb: () => void = () => {
+  return 'Hello'; //OK, no Error
+  //Caller mengabaikan hasil
+  //return value ga dianggap bagian dari kontrak
+};
+
+//Edge case: void type return undefined
+function f(): void {
+  return undefined; // OK, no Error
+  //undefined adalah absence of value
+  //Masih sesuai kontrak void
+}
