dfad4a53575dae62179b6e80537810d4e815aba9
vetacode <vetacode@gmail.com>
2025-12-31 23:07:40 +0700

    adds solution as const for tuple context

--- 4. More on Functions/9. restParametersArguments.ts
+++ 4. More on Functions/9. restParametersArguments.ts
@@ -15,3 +15,18 @@ arr1.push(...arr2); //spread operator -> memecah array (ambil isi array-nya satu
 //push() mengharapkan number, tapi arr2 adalah number[]
 //klo kita passing langsung arr2:
 arr1.push(arr2); //Error: Argument of type 'number[]' is not assignable to parameter of type 'number'
+
+// Inferred type is number[] -- "an array with zero or more numbers",
+// not specifically two numbers
+const args = [8, 5];
+const angle = Math.atan2(...args);
+// A spread argument must either have a tuple type or be passed to a rest parameter.
+
+//Solution: use 'as const' to inferred as 2-length tuple
+const args2 = [8, 5] as const; //give const context on args2 -> becomes tuple readonly
+const angle2 = Math.atan2(...args2);
+
+function addNum(a: number, ...b: number[]): number[] {
+  return b.map((y) => a + y);
+}
+console.log(addNum(2, 1, 2, 3, 4)); //3,4,5,6
