98eecf300672875423e59f9631828f307ebe5f40
vetacode <vetacode@gmail.com>
2026-01-02 16:32:53 +0700

    adds readonly property and slightly changes on narrowing

--- 3. Narrowing/2. typeofTypeguards.ts
+++ 3. Narrowing/2. typeofTypeguards.ts
@@ -12,7 +12,7 @@
 function printAll(strs: string | string[] | null) {
   if (typeof strs === 'object') {
     for (const s of strs) {
-      // 'strs' is possibly 'null'.
+      //              ^'strs' is possibly 'null'.
       console.log(s);
     }
   } else if (typeof strs === 'string') {
---
+++ 5. Object Types/2.2 readonlyProps.ts
@@ -0,0 +1,95 @@
+//readonly is a modifier
+//a property marked as readonly can’t be written to during type-checking.
+//it won’t change any behavior at runtime
+
+interface SomeType {
+  readonly prop: string;
+}
+
+function doSomething(obj: SomeType) {
+  // We can read from 'obj.prop'.
+  console.log(`prop has the value '${obj.prop}'.`);
+
+  // But we can't re-assign it.
+  obj.prop = 'hello';
+  //    ^ Cannot assign to 'prop' because it is a read-only property.
+}
+
+doSomething({ prop: 'Do anything' }); // prop has the value 'Do anything'.
+
+//readonly doesnt mean deeply immutable.
+interface Home {
+  readonly resident: { name: string; age: number };
+  // artinya:
+  // Properti resident tidak boleh diganti referensinya,
+  // TAPI object yang dirujuk oleh resident masih mutable.
+}
+
+function visitForBirthday(home: Home) {
+  // We can read and update properties from 'home.resident'.
+  console.log(`Happy birthday ${home.resident.name}!`);
+  home.resident.name = 'Eska'; //boleh mutasi isi obj
+  home.resident.age++; //boleh mutasi isi obj
+}
+
+function evict(home: Home) {
+  // But we can't write to the 'resident' property itself on a 'Home'.
+  home.resident = {
+    // Cannot assign to 'resident' because it is a read-only property.
+    // Ga boleh ganti obj baru
+    name: 'Victor the Evictor',
+    age: 42,
+  };
+}
+
+visitForBirthday({ resident: { name: 'Eska', age: 40 } }); //Happy birthday Eska!
+
+//Syntax destructuring:
+function visitForBirthday2({ resident }: Home) {
+  console.log(resident.name);
+}
+
+//OR nested
+function visitForBirthday3({ resident: { name, age } }: Home) {
+  console.log(name, age);
+}
+
+//Jika ingin totally deep immutable: pakai deep readonly
+interface House {
+  readonly resident: Readonly<{ name: string; age: number }>;
+}
+
+function visitParty(house: House) {
+  house.resident.name = 'John'; // sdh ga bs diakses/immutable
+  //               ^ Cannot assign to 'name' because it is a read-only property.
+}
+
+//Beda readonly vs as const
+//readonly melindungi struktur, bukan nilai
+//cocok digunakan untuk API/contract
+interface Home {
+  readonly resident: {
+    name: string;
+    age: number;
+  };
+}
+
+function visit(home: Home) {
+  home.resident.age++; //Aman
+  home.resident = {} as any; // Error: Cannot assign to 'resident' because it is a read-only property.
+}
+
+//as const: freeze nilai literal
+//menjadi deep readonly, semuanya readonly (deep immutable)
+//cocok digunakan untuk constants/config
+const home = {
+  resident: {
+    name: 'Eska',
+    age: 40,
+  },
+} as const;
+
+console.log(home.resident.name); //Eska
+console.log(home.resident.age); //40
+
+home.resident.age++; //error: Cannot assign to 'age' because it is a read-only property.
