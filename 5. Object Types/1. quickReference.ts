75dcb6015a20eec784ee4d2d13549f548af87252
vetacode <vetacode@gmail.com>
2026-01-02 11:27:22 +0700

    adds quick reference on Object types

---
+++ 5. Object Types/1. quickReference.ts
@@ -0,0 +1,29 @@
+//Object types
+function greet(person: { name: string; age: number }) {
+  return 'Hello ' + person.name + ', ' + 'Your age is ' + person.age;
+}
+console.log(greet({ name: 'Rina', age: 18 })); // Hello Rina, Your age is 18
+console.log(greet({ name: '', age: 18 })); // Hello , Your age is 18
+
+//OR using interface
+
+interface Person {
+  name: string;
+  age: number;
+}
+
+function greet2(person: Person) {
+  return 'Hello ' + person.name;
+}
+
+//OR using type alias
+type Person2 = {
+  name: string;
+  age: number;
+};
+
+function greet3(person: Person2) {
+  return 'Hello ' + person.name;
+}
+
+//In all three examples above, we’ve written functions that take objects that contain the property name (which must be a string) and age (which must be a number).
