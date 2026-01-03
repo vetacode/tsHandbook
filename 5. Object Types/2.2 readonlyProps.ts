bfa1a6c772b79b19bd42d0004e80333ed0824daa
vetacode <vetacode@gmail.com>
2026-01-03 16:08:17 +0700

    adds readonly props can also change via aliasing

--- 5. Object Types/2.2 readonlyProps.ts
+++ 5. Object Types/2.2 readonlyProps.ts
@@ -110,3 +110,28 @@ const ROUTES = {
 //Readonly<T>     // "jangan ganti property ini"
 // DeepReadonly<T> // "jangan ubah APAPUN"
 // as const        // "nilai ini mutlak & readonly"
+
+//readonly properties can also change via aliasing
+interface Person {
+  name: string;
+  age: number;
+}
+
+interface ReadonlyPerson {
+  readonly name: string;
+  readonly age: number;
+}
+
+let writablePerson: Person = {
+  name: 'Person McPersonface',
+  age: 42,
+};
+
+// works
+let readonlyPerson: ReadonlyPerson = writablePerson;
+
+console.log(readonlyPerson.age); // prints '42'
+writablePerson.age++;
+console.log(readonlyPerson.age); // prints '43'
+
+//REMARK: use mapping modifiers to remove readonly
