ed1c9f0975d9b4fe7b9ebc13697390c3832233ba
vetacode <vetacode@gmail.com>
2026-01-05 10:35:31 +0700

    adds interface extension vs intersection

---
+++ 5. Object Types/6. interfaceExtensionIntersection.ts
@@ -0,0 +1,38 @@
+//The same name of type will be merged
+interface Animal {
+  life: string;
+  species: string;
+}
+interface Animal {
+  health: number;
+}
+
+interface Dog {
+  skill: string;
+}
+
+// type Jenis = Dog & Animal;
+
+function whichAnimal(shout: Dog & Animal) {
+  console.log(
+    `The ${shout.species} has ${shout.health}% health, still ${shout.life}, and can ${shout.skill} `
+  );
+}
+
+whichAnimal({ life: 'Alive', species: 'Dog', health: 100, skill: 'bark' });
+
+//these interface merging will results in a never type:
+interface Person1 {
+  name: string;
+}
+
+interface Person2 {
+  name: number;
+}
+
+type Staff = Person1 & Person2;
+
+declare const staffer: Staff;
+staffer.name;
+//        ^ (property) name: never
+// coz Staff would require the name property to be both a string and a number
