501f83f5314ae9ca6548c0c6fb1b3e4809a41288
vetacode <vetacode@gmail.com>
2026-01-05 15:48:11 +0700

    adds solution to overwhelming overloads with generic type

--- 5. Object Types/7. genericObjectTypes.ts
+++ 5. Object Types/7. genericObjectTypes.ts
@@ -55,7 +55,7 @@ if (typeof x.contents === 'number') {
 const val = x.contents;
 typeof val === 'number' ? console.log(val * 2) : console.log(val.toUpperCase());
 
-//using generic type is same. as declaring the specific type with flexibility
+//using generic type is same as declaring the specific type with flexibility
 {
   interface Box<Type> {
     contents: Type;
@@ -71,4 +71,40 @@ typeof val === 'number' ? console.log(val * 2) : console.log(val.toUpperCase());
   let boxB: StringBox = { contents: 'world' };
   boxB.contents;
   //       ^(property) StringBox.contents: string
+}
+
+//with object generic type, we can avoid using overloads entirely
+interface NumberBox {
+  contents: number;
+}
+
+interface StringBox {
+  contents: string;
+}
+
+interface BooleanBox {
+  contents: boolean;
+}
+
+function setContents(box: StringBox, newContents: string): void;
+function setContents(box: NumberBox, newContents: number): void;
+function setContents(box: BooleanBox, newContents: boolean): void;
+function setContents(box: { contents: any }, newContents: any) {
+  box.contents = newContents;
+}
+
+//SOLUTION
+{
+  interface Box<T> {
+    contents: T;
+  }
+
+  function setContents<T>(box: Box<T>, newContents: T) {
+    box.contents = newContents;
+    return `I say ${box.contents}, and you also reply ${newContents}`;
+  }
+
+  console.log(setContents({ contents: 'Hello' }, 'World')); //I say World, and you also reply World
+  console.log(setContents({ contents: 100 }, 500)); //I say 500, and you also reply 500
+  console.log(setContents({ contents: true }, false)); //I say false, and you also reply false
 }
