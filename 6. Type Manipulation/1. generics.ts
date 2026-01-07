bb305c8dc6ca91dad69c6915f082ddf011f9c46c
vetacode <vetacode@gmail.com>
2026-01-07 12:16:58 +0700

    add generic types

--- 6. Type Manipulation/1. generics.ts
+++ 6. Type Manipulation/1. generics.ts
@@ -38,9 +38,35 @@ let outputStr2 = identity('myString');
 function arrLen<T>(...args: T[]): T[] {
   if (args.every((arg) => typeof arg === 'number')) {
     console.log(`Semua number, dengan length: ${args.length}`);
+  } else {
+    console.log(args);
   }
   return args;
 }
 arrLen<number>(100, 10, 25);
+arrLen<string>('apa', 'aba', 'ata');
 // let arr1 = arrLen<number>(100);
 // console.log(arr1);
+
+//versi lebih aman karena tdk return apapun (side effect function only)
+function arrLen2<T>(...args: T[]): void {
+  if (args.every((arg) => typeof arg === 'number')) {
+    console.log(`Semua number, dengan length: ${args.length}`);
+  } else {
+    console.log(args);
+  }
+}
+
+{
+  //3. Generic Types
+  // The type of generic functions is just like those of non-generic functions, with the type parameters listed first, similarly to function declarations:
+
+  function identity<T>(arg: T): T {
+    return arg;
+  }
+  let myIdentity: <T>(arg: T) => T = identity;
+  //myIdentity adalah sebuah function generic,
+  // yang punya type parameter: T,
+  // menerima arg: T,
+  // dan mengembalikan T.
+}
