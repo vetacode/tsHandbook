01c7d0db771b8484f4593452b5e878160184968b
vetacode <vetacode@gmail.com>
2026-01-08 23:12:39 +0700

    adds keyof type operators

---
+++ 6. Type Manipulation/2. keyofTypeOperator.ts
@@ -0,0 +1,18 @@
+//The keyof type operator
+//The keyof operator takes an object type and produces a string or numeric literal union of its keys
+//The following type P is the same type as type P = "x" | "y":
+
+type Point = { x: number; y: number };
+type P = keyof Point;
+//   ^ type P = keyof Point
+
+//If the type has a string or number index signature, keyof will return those types instead:
+type Arrayish = { [n: number]: unknown };
+type A = keyof Arrayish;
+//   ^ type A = number
+
+type Mapish = { [k: string]: boolean };
+type M = keyof Mapish;
+//   ^ type M = string | number
+// Note that in this example, M is string | number
+// this is because JavaScript object keys are always coerced to a string, so obj[0] is always the same as obj["0"].
