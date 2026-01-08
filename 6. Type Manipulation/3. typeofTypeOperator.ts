e85e440d76eb476dc298e81e8f26a9fff452891a
vetacode <vetacode@gmail.com>
2026-01-08 23:21:59 +0700

    completing typeof type operator

--- 6. Type Manipulation/3. typeofTypeOperator.ts
+++ 6. Type Manipulation/3. typeofTypeOperator.ts
@@ -6,3 +6,28 @@ let n: typeof s;
 type Predicate = (x: unknown) => boolean;
 type K = ReturnType<Predicate>;
 //   ^ type K = boolean
+
+//If we try to use ReturnType on a function name, we see an instructive error:
+function f() {
+  return { x: 10, y: 3 };
+}
+type P = ReturnType<f>;
+//                  ^ 'f' refers to a value, but is being used as a type here. Did you mean 'typeof f'?
+
+//Remember that values and types aren’t the same thing. To refer to the type that the value f has, we use typeof:
+function f2() {
+  return { x: 10, y: 3 };
+}
+type P2 = ReturnType<typeof f2>;
+// type P2 = {
+//     x: number;
+//     y: number;
+// }
+
+// Limitations
+// TypeScript intentionally limits the sorts of expressions you can use typeof on.
+
+// Specifically, it’s only legal to use typeof on identifiers (i.e. variable names) or their properties. This helps avoid the confusing trap of writing code you think is executing, but isn’t:
+// Meant to use = ReturnType<typeof msgbox>
+let shouldContinue: typeof msgbox("Are you sure you want to continue?");
+// ',' expected.
