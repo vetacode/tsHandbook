eeb5796999371045f2c2f7f4486a42cc057940c8
vetacode <vetacode@gmail.com>
2026-01-09 16:36:47 +0700

    adds solution to overloads using conditional types

--- 6. Type Manipulation/5. conditionalTypes.ts
+++ 6. Type Manipulation/5. conditionalTypes.ts
@@ -10,3 +10,39 @@ type Example1 = Dog extends Animal ? number : string;
 
 type Example2 = RegExp extends Animal ? number : string;
 // type Example2 = string
+
+//useful combination with generics
+interface IdLabel {
+  id: number /* some fields */;
+}
+interface NameLabel {
+  name: string /* other fields */;
+}
+
+function createLabel(id: number): IdLabel;
+function createLabel(name: string): NameLabel;
+function createLabel(nameOrId: string | number): IdLabel | NameLabel;
+function createLabel(nameOrId: string | number): IdLabel | NameLabel {
+  throw 'unimplemented';
+}
+
+//Solution: Use conditional type
+type K = number | string;
+type NameOrId<T extends K> = T extends number ? IdLabel : NameLabel;
+
+//then use that conditional type to simplify overloads
+function createLabel2<T extends K>(idOrName: T): NameOrId<T> {
+  throw 'unimplemented';
+}
+
+let a = createLabel2('typescript');
+//  ^ let a: NameLabel
+
+let b = createLabel(5);
+//  ^ let b: IdLabel
+
+let c = createLabel(Math.random() ? 'hello' : 42);
+//  ^ let c: NameLabel | IdLabel
+// Math.random() -> 0 <= n < 1
+// 0 → false
+// Semua angka selain 0 → true
