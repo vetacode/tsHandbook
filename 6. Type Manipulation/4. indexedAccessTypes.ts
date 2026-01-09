9f6b459aaec35cf84ff0f80dad2acfe8aa30832b
vetacode <vetacode@gmail.com>
2026-01-09 15:29:05 +0700

    adds computing type index

--- 6. Type Manipulation/4. indexedAccessTypes.ts
+++ 6. Type Manipulation/4. indexedAccessTypes.ts
@@ -9,3 +9,12 @@ type Age = Laki['age'];
 //    ^ type Age = number
 
 //indexing type is itself a type, so we can use unions, keyof, or other types entirely:
+type NameOrAlive = Laki['name' | 'alive'];
+//      ^ type NameOrAlive = string | boolean
+
+type LakiType = Laki[keyof Laki];
+//      ^ type LakiType = string | number | boolean
+
+type NameOrAge = 'name' | 'age';
+type Mixed = Laki[NameOrAge];
+//     ^ type Mixed = string | number
