09f624b20fda9b8c9f91059fc96d6aba70e359f4
vetacode <vetacode@gmail.com>
2026-01-09 16:01:35 +0700

    adds conditional types

---
+++ 6. Type Manipulation/5. conditionalTypes.ts
@@ -0,0 +1,12 @@
+interface Animal {
+  live(): void;
+}
+interface Dog extends Animal {
+  woof(): void;
+}
+
+type Example1 = Dog extends Animal ? number : string;
+// type Example1 = number
+
+type Example2 = RegExp extends Animal ? number : string;
+// type Example2 = string
