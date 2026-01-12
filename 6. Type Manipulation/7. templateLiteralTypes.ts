0d53be0e5e051c343120a36dfc6a724ba9d68b1d
vetacode <vetacode@gmail.com>
2026-01-12 22:07:36 +0700

    adds string union in types

--- 6. Type Manipulation/7. templateLiteralTypes.ts
+++ 6. Type Manipulation/7. templateLiteralTypes.ts
@@ -14,3 +14,46 @@ type Lang = 'en' | 'ja' | 'pt';
 
 type LocaleMessageIDs = `${Lang}_${AllLocaleIDs}`;
 // type LocaleMessageIDs = "en_welcome_email_id" | "en_email_heading_id" | "en_footer_title_id" | "en_footer_sendoff_id" | "ja_welcome_email_id" | "ja_email_heading_id" | "ja_footer_title_id" | "ja_footer_sendoff_id" | "pt_welcome_email_id" | "pt_email_heading_id" | "pt_footer_title_id" | "pt_footer_sendoff_id"
+
+//1. STRING UNIONS IN TYPES
+
+const passedObject = {
+  firstName: 'Saoirse',
+  lastName: 'Ronan',
+  age: 26,
+};
+
+const person = makeWatchedObject({
+  firstName: 'Saoirse',
+  lastName: 'Ronan',
+  age: 26,
+});
+
+// makeWatchedObject has added `on` to the anonymous Object
+
+person.on('firstNameChanged', (newValue) => {
+  console.log(`firstName was changed to ${newValue}!`);
+});
+
+type PropEventSource<Type> = {
+  on(
+    eventName: `${string & keyof Type}Changed`,
+    callback: (newValue: any) => void
+  ): void;
+};
+
+/// Create a "watched object" with an `on` method
+/// so that you can watch for changes to properties.
+declare function makeWatchedObject<Type>(
+  obj: Type
+): Type & PropEventSource<Type>;
+
+person.on('firstNameChanged', () => {});
+
+// Prevent easy human error (using the key instead of the event name)
+person.on('firstName', () => {});
+// Argument of type '"firstName"' is not assignable to parameter of type '"firstNameChanged" | "lastNameChanged" | "ageChanged"'.
+
+// It's typo-resistant
+person.on('frstNameChanged', () => {});
+// Argument of type '"frstNameChanged"' is not assignable to parameter of type '"firstNameChanged" | "lastNameChanged" | "ageChanged"'.
