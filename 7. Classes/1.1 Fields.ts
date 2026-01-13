518216148954309bd3a558d7cad64213c2b4a060
vetacode <vetacode@gmail.com>
2026-01-13 10:45:18 +0700

    adds fields

--- 7. Classes/1.1 Fields.ts
+++ 7. Classes/1.1 Fields.ts
@@ -7,3 +7,35 @@ class Point {
 const pt = new Point();
 pt.x = 0;
 pt.y = 0;
+
+class Point2 {
+  x = 0;
+  y = 0;
+}
+
+const pt2 = new Point2();
+console.log(`${pt2.x}, ${pt2.y}`); //0, 0
+
+//initializer of a class property will be used to infer its type
+pt2.x = 1; // OK -> type number
+pt2.x = '1'; //Error: Type 'string' is not assignable to type 'number'.
+
+//--strictPropertyInitialization
+//if sets true (or strict):
+class Sports2 {
+  name: string; // Error: Property 'name' has no initializer and is not definitely assigned in the constructor.
+}
+
+//Solution: create constructor to initialize
+class Sports {
+  name: string;
+
+  constructor() {
+    this.name = 'Soccer';
+  }
+}
+
+class OKGreeter {
+  // Not initialized, but no error
+  name!: string;
+}
