4f5e7c4f8a7d9153a94366de0633abe320875e38
vetacode <vetacode@gmail.com>
2026-01-14 07:08:13 +0700

    adds index signatures on types

---
+++ 7. Classes/1.6 indexSignatures.ts
@@ -0,0 +1,10 @@
+//Classes can declare index signatures; these work the same as Index Signatures for other object types:
+class MyClass {
+  [s: string]: boolean | ((s: string) => boolean);
+
+  check(s: string) {
+    return this[s] as boolean;
+  }
+}
+
+//Because the index signature type needs to also capture the types of methods, it’s not easy to usefully use these types. Generally it’s better to store indexed data in another place instead of on the class instance itself.
