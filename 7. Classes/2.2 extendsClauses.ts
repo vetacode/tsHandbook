7179e0480aba17c86b3b027976989652f063bac6
vetacode <vetacode@gmail.com>
2026-01-15 11:22:34 +0700

    adds derived dindnt follow base error

--- 7. Classes/2.2 extendsClauses.ts
+++ 7. Classes/2.2 extendsClauses.ts
@@ -52,3 +52,55 @@ b.greet(); // -> b ──► [ Derived instance ]
 const c: Derived = derived;
 c.greet(); //aman
 c.greet('Hellow'); //aman
+
+{
+  //Jika Derived (Subclass) didn’t follow Base’s contract:
+  class Base {
+    greet() {
+      console.log('Hello, world!');
+    }
+  }
+
+  class Derived extends Base {
+    // Make this parameter required
+    greet(name: string | undefined) {
+      // -> pakai union | undefined jg tdk bisa coz beda dgn optional params '?'
+      // Property 'greet' in type 'Derived' is not assignable to the same property in base type 'Base'.
+      //   Type '(name: string) => void' is not assignable to type '() => void'.
+      //     Target signature provides too few arguments. Expected 1 or more, but got 0. -> Base boleh dipanggil tanpa argumen, Derived tidak bisa menerima pemanggilan itu -> Derived tidak kompatibel sebagai Base
+      if (name === undefined) {
+        super.greet;
+      } else {
+        console.log(`Hello, ${name.toUpperCase()}`);
+      }
+    }
+  }
+  //Artinya:
+  // Base menjanjikan fungsi yang bisa dipanggil TANPA argumen, tapi Derived malah minta 1 argumen WAJIB.
+  const d = new Derived();
+  const b: Base = d;
+  b.greet();
+}
+
+{
+  class Base {
+    greet() {
+      console.log('Hello, world!');
+    }
+  }
+
+  class Derived extends Base {
+    greet(name: string | undefined) {
+      // -> pakai union | undefined jg tdk bisa coz beda dgn optional params '?'
+      if (name === undefined) {
+        super.greet;
+      } else {
+        console.log(`Hello, ${name.toUpperCase()}`);
+      }
+    }
+  }
+
+  const d = new Derived();
+  const b: Base = d;
+  b.greet();
+}
