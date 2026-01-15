791da53a95229124ab1b27275ae6f57ed2bed5d9
vetacode <vetacode@gmail.com>
2026-01-15 08:29:37 +0700

    separats topic class heritage to each point

---
+++ 7. Classes/2.1 implementClauses.ts
@@ -0,0 +1,73 @@
+interface Pingable {
+  ping(): void;
+}
+
+class Sonar implements Pingable {
+  ping() {
+    console.log('ping!');
+  }
+}
+//Yg TS lakukan adlh nge-cek:
+// Apakah class Sonar punya method ping()? yes -> aman
+// Apakah return type-nya cocok (void)? yes -> aman
+
+//contoh error:
+class Ball implements Pingable {
+  // Class 'Ball' incorrectly implements interface 'Pingable'.
+  //   Property 'ping' is missing in type 'Ball' but required in type 'Pingable'.
+  pong() {
+    console.log('pong!');
+  }
+
+  // ping() {
+  //   console.log('ping');
+  // }
+}
+//Error krn:
+// interface minta ping() -> tp class ga punya ping()
+
+//Kesalahan umum: implements mengubah type parameter
+interface Checkable {
+  check(name: string): boolean;
+}
+
+class NameChecker implements Checkable {
+  check(s) {
+    // Parameter 's' implicitly has an 'any' type.
+    // Notice no error here
+    return s.toLowerCase() === 'ok';
+
+    // s: any -> coz implements interface ga passing/mengubah type ke class
+  }
+}
+
+//Solusi: tulis manual type di class
+class NameChecker2 implements Checkable {
+  check(s: string) {
+    return s.toLocaleLowerCase() === 'ok';
+  }
+}
+//interface hanya ngecek kecocokan, ga ngerubah apapun
+//Artinya: implements hanya ngecek: Apakah check() PUNYA parameter string dan return boolean?
+
+//Optional Props tidak otomatis dibuat
+interface A {
+  x: number;
+  y?: number;
+}
+class C implements A {
+  x = 0;
+}
+const c = new C();
+c.y = 10;
+// Property 'y' does not exist on type 'C'.
+//implements tidak otomatis menambahkan properti 'y'
+
+//Solusi: sertakan optional type
+class C2 implements A {
+  x = 0;
+  y?: number;
+}
+
+const c2 = new C2();
+c2.y = 10; //aman
