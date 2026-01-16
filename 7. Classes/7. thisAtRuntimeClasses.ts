97d8bb09793b48d61a07403f31d1cbaf4f754964
vetacode <vetacode@gmail.com>
2026-01-16 17:08:24 +0700

    adds this at runtime in classes

---
+++ 7. Classes/7. thisAtRuntimeClasses.ts
@@ -0,0 +1,26 @@
+//JavaScript’s handling of this is indeed unusual:
+class MyClass {
+  name = 'Kelas';
+  getName() {
+    return this.name;
+  }
+}
+const c = new MyClass();
+// c adalah instance dari MyClass
+// c.name → "MyClass"
+// c.getName() → method yang membaca this.name
+
+const obj = {
+  name: 'Ojek',
+  getName: c.getName,
+};
+
+// Prints "Ojek", not "Kelas"
+console.log(obj.getName()); //Ojek
+//Karena di JS:
+// this tidak “menempel” ke fungsi
+// this ditentukan oleh SIAPA YANG MEMANGGIL fungsi itu
+// -> this === obj -> return this.name -> obj.name -> Ojek
+
+c.name; // -> Kelas
+c.getName(); // -> this === c -> return this.name -> c.name -> Kelas
