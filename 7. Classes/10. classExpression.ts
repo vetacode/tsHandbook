21fb166e607e33dbdb6b596eaf9c576a4c506b41
vetacode <vetacode@gmail.com>
2026-01-18 22:09:14 +0700

    adds example that have to use class expression

--- 7. Classes/10. classExpression.ts
+++ 7. Classes/10. classExpression.ts
@@ -35,3 +35,16 @@ const someClass = class<T> {
 
 const m = new someClass('Hello, world');
 // const m: someClass<string>;
+
+{
+  //Contoh yg tdk bs pakai class declaration
+  function createClass<T>() {
+    return class {
+      //ini adalah expression
+      value!: T;
+    };
+  }
+
+  const MyClass = createClass<number>();
+  const x = new MyClass();
+}
