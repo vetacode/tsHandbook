bf926c9df327bbaba510c7e7939dd8ccb1e3ea62
vetacode <vetacode@gmail.com>
2026-01-22 10:56:14 +0700

    finalizing instance type

--- 9. Reference/1.14 instance.ts
+++ 9. Reference/1.14 instance.ts
@@ -15,7 +15,6 @@ class Mobil {
   }
 
   type T0 = InstanceType<typeof C>; //type T0 = C;
-
   //typeof C → tipe constructor dari class C (harus constructor type)
   // InstanceType<typeof C> → tipe objek hasil new C()
 }
@@ -26,3 +25,36 @@ class Mobil {
     T extends abstract new (...args: any) => infer R ? R : any;
   //T harus bisa dipanggil dengan new
 }
+
+//Aplikasi di project:
+//1. Framework/Factory
+function create<T extends new (...args: any[]) => any>(
+  Ctor: T,
+): InstanceType<T> {
+  return new Ctor();
+}
+//Return type otomatis mengikuti class yang dikirim
+
+//Generic helper untuk class
+class User {
+  name = 'A';
+}
+
+class Product {
+  price = 100;
+}
+
+type UserInstance = InstanceType<typeof User>;
+type ProductInstance = InstanceType<typeof Product>;
+
+//Library typing
+function register<T extends abstract new (...args: any) => any>(ctor: T) {
+  type Instance = InstanceType<T>;
+}
+
+// | Utility                    | Input       | Output          |
+// | -------------------------- | ----------- | --------------- |
+// | `Parameters<F>`            | function    | tuple parameter |
+// | `ReturnType<F>`            | function    | return value    |
+// | `ConstructorParameters<C>` | constructor | parameter `new` |
+// |   `InstanceType<C>`        | constructor | **instance**    |
