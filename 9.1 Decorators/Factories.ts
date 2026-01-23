777a218e241f53dd2451a47597e9eb96943b161b
vetacode <vetacode@gmail.com>
2026-01-23 10:16:24 +0700

    adds decorator setting and Factories ts

---
+++ 9.1 Decorators/Factories.ts
@@ -0,0 +1,12 @@
+function sealed(target) {
+  // do something with 'target' ...
+}
+
+function color(value: string) {
+  // this is the decorator factory, it sets up
+  // the returned decorator function
+  return function (target) {
+    // this is the decorator
+    // do something with 'target' and 'value'...
+  };
+}
