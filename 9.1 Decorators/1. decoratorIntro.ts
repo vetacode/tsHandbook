2df6ab74975d79f7358479083b68a3d12bc470d3
vetacode <vetacode@gmail.com>
2026-01-23 13:19:11 +0700

    adds decorator running steps

--- 9.1 Decorators/1. decoratorIntro.ts
+++ 9.1 Decorators/1. decoratorIntro.ts
@@ -15,9 +15,34 @@ function sealed(target) {
 // Kalau dipakai di class, maka:
 //  target = constructor function dari class itu
 
-//Contoh lain:
+//Bagaimana decorator diproses dan dijalankan:
 @sealed
 class User {}
 
 //Saat runtime
 sealed(User);
+//Decorator langsung dieksekusi, ga perlu nunggu object dibuat/dipanggil (new User()).
+
+{
+  //Mental model Urutan eksekusinya:
+  function sealed(target) {
+    console.log('Decorator dijalankan');
+  }
+
+  @sealed
+  class User {
+    constructor() {
+      console.log('Constructor dijalankan');
+    }
+  }
+
+  console.log('Sebelum new');
+  new User();
+  console.log('Sesudah new');
+
+  //Urutan Hasil log:
+  // Decorator dijalankan
+  // Sebelum new
+  // Constructor dijalankan
+  // Sesudah new
+}
