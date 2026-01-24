9ca2a114165ab5352f1c76f9cda42a7abc05904e
vetacode <vetacode@gmail.com>
2026-01-24 20:32:40 +0700

    finalizing method decorators

--- 9.1 Decorators/1.5 method.ts
+++ 9.1 Decorators/1.5 method.ts
@@ -11,42 +11,53 @@ class Greeter {
 // Decorator ini tidak nunggu object dibuat, tapi langsung dieksekusi saat class didefinisikan (runtime load).
 
 //Method decorator bekerja pada Property Descriptor dari method tersebut (metadata” JavaScript tentang sebuah property/method):
-{
-  value: function greet() {},
-  writable: true,
-  enumerable: true,
-  configurable: true
-}
+// {
+//   value: function greet() {},
+//   writable: true,
+//   enumerable: true,
+//   configurable: true
+// }
 //Jadi decorator bisa:
 // mengamati method
 // mengubah behavior method
 // bahkan mengganti method sepenuhnya
 
 {
-//Contoh @enumerable(false)
-class Greeter {
-  greeting: string;
+  //Contoh @enumerable(false)
+  class Greeter {
+    greeting: string;
 
-  constructor(message: string) {
-    this.greeting = message;
+    constructor(message: string) {
+      this.greeting = message;
+    }
+
+    @enumerable(false)
+    greet() {
+      return 'Hello, ' + this.greeting;
+    }
   }
 
-  @enumerable(false)
-  greet() {
-    return "Hello, " + this.greeting;
+  //Implementasi decorator
+  function enumerable(value: boolean) {
+    return function (
+      target: any,
+      propertyKey: string,
+      descriptor: PropertyDescriptor,
+    ) {
+      descriptor.enumerable = value;
+    };
   }
 }
 
-//Implementasi decorator
-function enumerable(value: boolean) {
-  return function (
-    target: any,
-    propertyKey: string,
-    descriptor: PropertyDescriptor
-  ) {
-    descriptor.enumerable = value;
+function deco() {
+  return function (target: any, key: string, descriptor: PropertyDescriptor) {
+    console.log(key);
   };
 }
 
-
+class A {
+  @deco()
+  foo() {
+    return 1;
+  }
 }
