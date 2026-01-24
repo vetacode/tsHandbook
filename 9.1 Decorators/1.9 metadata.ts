3966ceca978b79b56aaa1e051f1d08e2b1da79a9
vetacode <vetacode@gmail.com>
2026-01-24 20:42:34 +0700

    adds metadata

---
+++ 9.1 Decorators/1.9 metadata.ts
@@ -0,0 +1,84 @@
+//tsconfig.json:
+
+{
+  "compilerOptions": {
+    "target": "ES5",
+    "experimentalDecorators": true,
+    "emitDecoratorMetadata": true
+  }
+}
+
+
+import "reflect-metadata";
+ 
+class Point {
+  constructor(public x: number, public y: number) {}
+}
+ 
+class Line {
+  private _start: Point;
+  private _end: Point;
+ 
+  @validate
+  set start(value: Point) {
+    this._start = value;
+  }
+ 
+  get start() {
+    return this._start;
+  }
+ 
+  @validate
+  set end(value: Point) {
+    this._end = value;
+  }
+ 
+  get end() {
+    return this._end;
+  }
+}
+ 
+function validate<T>(target: any, propertyKey: string, descriptor: TypedPropertyDescriptor<T>) {
+  let set = descriptor.set!;
+  
+  descriptor.set = function (value: T) {
+    let type = Reflect.getMetadata("design:type", target, propertyKey);
+ 
+    if (!(value instanceof type)) {
+      throw new TypeError(`Invalid type, got ${typeof value} not ${type.name}.`);
+    }
+ 
+    set.call(this, value);
+  };
+}
+ 
+const line = new Line()
+line.start = new Point(0, 0)
+ 
+// @ts-ignore
+// line.end = {}
+ 
+// Fails at runtime with:
+// > Invalid type, got object not Point
+
+
+class Line {
+  private _start: Point;
+  private _end: Point;
+  @validate
+  @Reflect.metadata("design:type", Point)
+  set start(value: Point) {
+    this._start = value;
+  }
+  get start() {
+    return this._start;
+  }
+  @validate
+  @Reflect.metadata("design:type", Point)
+  set end(value: Point) {
+    this._end = value;
+  }
+  get end() {
+    return this._end;
+  }
+}
