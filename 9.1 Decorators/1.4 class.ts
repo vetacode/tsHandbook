4f26a74283b6443ce459a5c6a5bd3329fe09d830
vetacode <vetacode@gmail.com>
2026-01-24 15:56:31 +0700

    finalizing class decorators

--- 9.1 Decorators/1.4 class.ts
+++ 9.1 Decorators/1.4 class.ts
@@ -24,3 +24,32 @@ function sealed(constructor: Function) {
 
 //Saat runtime:
 // Class didefinisikan ->  Decorator dipanggil (sealed(BugReport))
+
+//example of how to override the constructor to set new defaults.
+function reportableClassDecorator<T extends { new (...args: any[]): {} }>(
+  constructor: T,
+) {
+  return class extends constructor {
+    reportingURL = 'http://www...';
+  };
+}
+
+@reportableClassDecorator
+class BugReport {
+  type = 'report';
+  title: string;
+
+  constructor(t: string) {
+    this.title = t;
+  }
+}
+
+const bug = new BugReport('Needs dark mode');
+console.log(bug.title); // Prints "Needs dark mode"
+console.log(bug.type); // Prints "report"
+
+// Note that the decorator _does not_ change the TypeScript type
+// and so the new property `reportingURL` is not known
+// to the type system:
+bug.reportingURL;
+// Property 'reportingURL' does not exist on type 'BugReport'.
