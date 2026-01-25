f55e88d8303124509758ef30fc0f20e97d3ef7e3
vetacode <vetacode@gmail.com>
2026-01-25 22:29:57 +0700

    finalizing declaration merging with classes Function Enums

--- 11. Declaration Merging/4. withClassesFuncEnum.ts
+++ 11. Declaration Merging/4. withClassesFuncEnum.ts
@@ -22,3 +22,60 @@ namespace Album {
 //Kenapa export wajib? coz Namespace punya aturan visibility -> Kalau ga export, class Album ga bisa melihatnya
 
 //2. Namespace + Class = Static Member Tambahan
+//Namespace bisa dipakai untuk:
+// Menambah static property
+// Menambah static helper
+
+class User {}
+
+namespace User {
+  export const role = 'admin';
+}
+
+//usage:
+User.role;
+
+//3. Merging Namespace dengan Function
+function buildLabel(name: string): string {
+  return buildLabel.prefix + name + buildLabel.suffix;
+}
+
+namespace buildLabel {
+  export let suffix = '';
+  export let prefix = 'Hello, ';
+}
+//Yang Digabung:
+// buildLabel() → function
+// buildLabel.prefix → property
+// buildLabel.suffix → property
+
+//Hasil akhir:
+buildLabel('Sam Smith');
+// "Hello, Sam Smith"
+
+//4. Merging Namespace dengan Enum
+enum Color {
+  red = 1,
+  green = 2,
+  blue = 4,
+}
+
+namespace Color {
+  export function mixColor(colorName: string) {
+    if (colorName == 'yellow') {
+      return Color.red + Color.green;
+    } else if (colorName == 'white') {
+      return Color.red + Color.green + Color.blue;
+    }
+  }
+}
+
+//Yang terjadi
+Color.red; // enum value
+Color.mixColor('redyew'); // function
+
+// | Target   | Namespace Dipakai Untuk    |
+// | -------- | -------------------------- |
+// | Class    | Inner class, static member |
+// | Function | Property & config          |
+// | Enum     | Helper & logic tambahan    |
