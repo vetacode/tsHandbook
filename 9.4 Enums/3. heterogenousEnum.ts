17d384dbd0379a6c35223588cf5d1ff135d3d105
vetacode <vetacode@gmail.com>
2026-01-26 11:56:10 +0700

    adds heterogenous enum

---
+++ 9.4 Enums/3. heterogenousEnum.ts
@@ -0,0 +1,5 @@
+enum BooleanLikeHeterogeneousEnum {
+  No = 0,
+  Yes = 'YES',
+}
+//Unless you’re really trying to take advantage of JavaScript’s runtime behavior in a clever way, it’s advised that you don’t do this.
