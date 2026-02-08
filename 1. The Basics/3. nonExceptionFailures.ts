ca9d77bc2da2b96229644481509956f92ed742ac
vetacode <vetacode@gmail.com>
2026-02-08 08:34:57 +0700

    updates basic logic errors

--- 1. The Basics/3. nonExceptionFailures.ts
+++ 1. The Basics/3. nonExceptionFailures.ts
@@ -20,13 +20,28 @@ function flipCoin() {
   // Meant to be Math.random()
   return Math.random < 0.5;
   // Operator '<' cannot be applied to types '() => number' and 'number'.
+  //yang dibandingkan adalah:
+  // fungsi (() => number dengan angka (0.5) => Makanya TypeScript protes
+}
+
+//Yang betul
+function flipCoin2() {
+  return Math.random() < 0.5;
+  //Sekarang alurnya: Math.random() → menghasilkan angka (misalnya 0.32)
+  // 0.32 < 0.5 → true
+  // Return true atau false
 }
 
 //4. Basic logic errors
 const value = Math.random() < 0.5 ? 'a' : 'b';
+//TypeScript menyimpulkan tipe value sebagai: union literal 'a' | 'b', bukan string
+
 if (value !== 'a') {
+  //disini TS baca hasil value === 'b'
   // ...
 } else if (value === 'b') {
+  //else if (dari 'b') berarti => 'a'
+  //disini TS membaca 'a' === 'b' => mustahil, sehingga error:
   // This comparison appears to be unintentional because the types '"a"' and '"b"' have no overlap.
   // Oops, unreachable
 }
