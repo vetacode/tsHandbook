51af8f638a8f79879db4ef466f67754c54cf0e57
vetacode <vetacode@gmail.com>
2026-02-11 13:14:18 +0700

    adds array destructuring

--- 9.14 Variable Declaration/5. destructuring.ts
+++ 9.14 Variable Declaration/5. destructuring.ts
@@ -5,21 +5,19 @@
   let [first, second] = input;
   console.log(first); // outputs 1
   console.log(second); // outputs 2
-  // This creates two new variables named first and second. This is equivalent to using indexing, but is much more convenient:
-}
-
-{
+  // This creates two new variables named first and second.
+  // This is equivalent to using indexing, but is much more convenient:
   first = input[0];
   second = input[1];
-  // Destructuring works with already-declared variables as well:
 }
 
 {
+  // Destructuring works with already-declared variables as well:
   // swap variables
   [first, second] = [second, first];
-  // And with parameters to a function:
 }
 
+// And with parameters to a function:
 function f([first, second]: [number, number]) {
   console.log(first);
   console.log(second);
@@ -34,8 +32,8 @@ console.log(rest); // outputs [ 2, 3, 4 ]
 
 let [first] = [1, 2, 3, 4];
 console.log(first); // outputs 1
-// Or other elements:
 
+// Or other elements:
 let [, second, , fourth] = [1, 2, 3, 4];
 console.log(second); // outputs 2
 console.log(fourth); // outputs 4
